e8aa504ced605ad8c000e2468173891d0bedfc3b
iMadfxq <imadfxq@gmail.com>
2022-09-15 08:01:03 -0400

    Finally. Delete functionality now completely working

--- src/Components/ItemsList/ItemsList.component.jsx
+++ src/Components/ItemsList/ItemsList.component.jsx
@@ -3,21 +3,43 @@ import "./ItemsList.styles.scss";
 import { useContext } from "react";
 import { PopupContext } from "../../Contexts/PopupContext";
 
-import ListItem from "../ListItem/ListItem.component";
+import { POPUP_ACTION_TYPES } from "../../Contexts/PopupContext";
 
 export default function ItemsList() {
   const { popupIsOpen, type, data, setterFunc, openPopup } =
     useContext(PopupContext);
+
+  const deleteHandler = (e) => {
+    e.stopPropagation();
+    const newList = data.filter((item) => {
+      if (item.id == e.target.parentElement.dataset.id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setterFunc(newList);
+    if (type === "ingredientsList") {
+      openPopup(POPUP_ACTION_TYPES.OPEN_POPUP_INGREDIENTS, {data: newList, setterFunc});
+    } else if (type === "stepsList") {
+      openPopup(POPUP_ACTION_TYPES.OPEN_POPUP_STEPS, {data: newList, setterFunc});
+    }
+  };
+
+  const completeHandler = (e) => {
+    e.target.parentElement.classList.toggle("completed");
+  };
 
   if (type === "ingredientsList") {
     return (
       <ul>
         {data.length &&
           data.map((ing) => (
-            <ListItem
-              key={ing.id}
-              item={ing}
-            />
+            <li data-id={ing.id}>
+              <span onClick={completeHandler}>✅</span>
+              <p>{ing.text}</p>
+              <span onClick={deleteHandler}>❌</span>
+            </li>
           ))}
       </ul>
     );
@@ -26,10 +48,11 @@ export default function ItemsList() {
       <ol>
         {data.length &&
           data.map((stp) => (
-            <ListItem
-              key={stp.id}
-              item={stp}
-            />
+            <li data-id={stp.id}>
+              <span onClick={completeHandler}>✅</span>
+              <p>{stp.text}</p>
+              <span onClick={deleteHandler}>❌</span>
+            </li>
           ))}
       </ol>
     );
--- src/Components/Popup/Popup.component.jsx
+++ src/Components/Popup/Popup.component.jsx
@@ -4,23 +4,12 @@ import { useContext } from "react";
 import { PopupContext } from "../../Contexts/PopupContext";
 import { POPUP_ACTION_TYPES } from "../../Contexts/PopupContext";
 import ItemsList from "../ItemsList/ItemsList.component";
-import ListItem from "../ListItem/ListItem.component";
 
 import "./Popup.styles.scss";
 
 export default function Popup() {
   const { popupIsOpen, type, data, setterFunc, openPopup } = useContext(PopupContext);
   
-  const [localListOfItems, setLocalListOfItems] = useState(data)
-
-  useEffect(() => {
-    setLocalListOfItems(data)
-    setterFunc([...data])
-  }, [data])
-
-  const closePopupHandler = () => {
-    openPopup(POPUP_ACTION_TYPES.CLOSE_POPUP, localListOfItems)
-  }
   
   if (popupIsOpen) {
         return (
