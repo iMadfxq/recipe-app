29331938167688f57449503fd0ab0a1736920151
iMadfxq <imadfxq@gmail.com>
2022-10-12 22:54:13 -0400

    started redesign home page

--- src/routes/Home/Home.component.jsx
+++ src/routes/Home/Home.component.jsx
@@ -1,5 +1,16 @@
-import './Home.styles.scss'
+import "./Home.styles.scss";
+
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <p>Home</p>
+  return (
+    <main>
+      <h2>React Recipe App</h2>
+      <section>
+        <p>Recipes suggested by iMadfxq: </p>
+        <p style={{fontSize: '10px'}}>**List of recipes from firebase**</p>
+      </section>
+      <Link to='/recipes'>See all recipes</Link>
+    </main>
+  );
 }
