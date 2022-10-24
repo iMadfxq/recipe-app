58442649476d2cc7ac5d792464295d2e3f74b72c
iMadfxq <imadfxq@gmail.com>
2022-10-24 00:50:58 -0400

    Redirects and reload solving issues

--- src/routes/RecipeList/RecipeList.component.jsx
+++ src/routes/RecipeList/RecipeList.component.jsx
@@ -7,61 +7,109 @@ import SuggestedRecipes from "../../Components/SuggestedRecipes/SuggestedRecipes
 import { RecipesContext } from "../../Contexts/RecipesContext";
 import { UserContext } from "../../Contexts/UserContext";
 import { projectFirestore } from "../../firebase/config";
+import { useEffect } from "react";
 
 export default function RecipeList({ recipesList }) {
   const { data } = useContext(RecipesContext);
   const { machineId } = useContext(UserContext);
 
-  let userCreatedRecipes = data.filter(
-    (recipe) => recipe.machineId == machineId
-  );
+  let userCreatedRecipes = [];
+  let otherPeopleCreatedRecipes = [];
 
-  let otherPeopleCreatedRecipes = data.filter((r) => r.machineId != machineId && r.byDeveloper == false)
+
+  if(data && data.length) {
+    otherPeopleCreatedRecipes = data.filter(
+      (r) => r.machineId != machineId && r.byDeveloper == false
+    );
+    userCreatedRecipes = data.filter(
+      (recipe) => recipe.machineId == machineId
+    );
+  }
+ 
 
   return (
     <main className="RecipeList">
       <h1>All recipes: </h1>
-      <SuggestedRecipes />
-      <h2>Created by you:</h2>
-      <section className="RecipeList__user">
-        {userCreatedRecipes.length ? userCreatedRecipes.map((recipe) => (
-          <Link
-            to={`/Recipes/${recipe.id}`}
-            className="RecipeList__user--item"
-            key={recipe.id}
-          >
-            <span onClick={(e) => {
-              e.preventDefault()
-              projectFirestore.collection('suggestedRecipes').doc(recipe.id).delete()
-            }} title='Delete' >❌</span>
-            <h3>{recipe.title}</h3>
-            <p>Cooking time: <span>{recipe.cookingTime} minutes</span></p>
-            <p>Number of ingredients: <span> {recipe.ingredientsList.length}</span></p>
-            
-              <p>Number of steps: <span>{recipe.stepsList.length}</span> </p>
-            
-            <button>See recipe</button>
-          </Link>
-        )) : <p style={{fontSize: '2rem', textAlign:'center'}}>You haven't created any recipes yet :(</p>}
-      </section>
-      <h2>Created by other users: </h2>
-      <section className="RecipeList__user">
-        {otherPeopleCreatedRecipes.length ? 
-          otherPeopleCreatedRecipes.map((recipe) => (
-            <Link
-              to={`/Recipes/${recipe.id}`}
-              className="RecipeList__user--item"
-              key={recipe.id}
-            >
-              <h3>{recipe.title}</h3>
-              <p>Cooking time: <span> {recipe.cookingTime} minutes</span></p>
-              <p>Number of ingredients: <span>{recipe.ingredientsList.length}</span> </p>
-              
-                <p>Number of steps: <span> {recipe.stepsList.length}</span></p>
-              <button>See recipe</button>
-            </Link>
-          )) : <p style={{fontSize: '2rem', textAlign:'center'}}>There are no recipes created by other users :(</p>}
-      </section>
+      {data && data.length ? (
+        <>
+          <SuggestedRecipes />
+          <h2>Created by you:</h2>
+          <section className="RecipeList__user">
+            {userCreatedRecipes.length ? (
+              userCreatedRecipes.map((recipe) => (
+                <Link
+                  to={`/Recipes/${recipe.id}`}
+                  className="RecipeList__user--item"
+                  key={recipe.id}
+                >
+                  <span
+                    onClick={(e) => {
+                      e.preventDefault();
+                      projectFirestore
+                        .collection("suggestedRecipes")
+                        .doc(recipe.id)
+                        .delete();
+                    }}
+                    title="Delete"
+                  >
+                    ❌
+                  </span>
+                  <h3>{recipe.title}</h3>
+                  <p>
+                    Cooking time: <span>{recipe.cookingTime} minutes</span>
+                  </p>
+                  <p>
+                    Number of ingredients:{" "}
+                    <span> {recipe.ingredientsList.length}</span>
+                  </p>
+
+                  <p>
+                    Number of steps: <span>{recipe.stepsList.length}</span>{" "}
+                  </p>
+
+                  <button>See recipe</button>
+                </Link>
+              ))
+            ) : (
+              <p style={{ fontSize: "2rem", textAlign: "center" }}>
+                You haven't created any recipes yet :(
+              </p>
+            )}
+          </section>
+          <h2>Created by other users: </h2>
+          <section className="RecipeList__user">
+            {otherPeopleCreatedRecipes.length ? (
+              otherPeopleCreatedRecipes.map((recipe) => (
+                <Link
+                  to={`/Recipes/${recipe.id}`}
+                  className="RecipeList__user--item"
+                  key={recipe.id}
+                >
+                  <h3>{recipe.title}</h3>
+                  <p>
+                    Cooking time: <span> {recipe.cookingTime} minutes</span>
+                  </p>
+                  <p>
+                    Number of ingredients:{" "}
+                    <span>{recipe.ingredientsList.length}</span>{" "}
+                  </p>
+
+                  <p>
+                    Number of steps: <span> {recipe.stepsList.length}</span>
+                  </p>
+                  <button>See recipe</button>
+                </Link>
+              ))
+            ) : (
+              <p style={{ fontSize: "2rem", textAlign: "center" }}>
+                There are no recipes created by other users :(
+              </p>
+            )}
+          </section>
+        </>
+      ) : (
+        <></>
+      )}
     </main>
   );
 }
